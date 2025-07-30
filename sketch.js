c1f2ce4ca80c681a1fae0d9746cc53ebb1909671
theus <141179554+zangadodaana@users.noreply.github.com>
2025-07-30 13:44:37 -0300

    n

--- sketch.js
+++ sketch.js
@@ -4,7 +4,7 @@ function setup() {
 }
 
 function draw() {
-  background(205, 102, 94); //cor de fundo
+  background(208, 104, 93); //cor de fundo
   rotateY(millis()/2000); // rotação da terra
   texture(terra); //textura da terra, imagem.
   sphere(400, 300); //esfera 3D
